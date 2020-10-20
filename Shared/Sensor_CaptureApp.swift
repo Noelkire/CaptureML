bda573154fdcd74ede005bdb31635bf9d069784a
Erik Leon <erikgleon@gmail.com>
2020-10-20 14:21:23 -0700

    Initial Commit

---
+++ Shared/Sensor_CaptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sensor_CaptureApp.swift
+//  Shared
+//
+//  Created by Erik Leon on 10/20/20.
+//
+
+import SwiftUI
+
+@main
+struct Sensor_CaptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
